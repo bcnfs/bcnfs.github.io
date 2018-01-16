0f2d7ffa87740db123321a855644c18e3cc69340
Martín González <m@martingonzalez.net>
2018-01-16 21:01:28 +0000

    Add upcoming meetups to the list

--- js/main.js
+++ js/main.js
@@ -4,17 +4,30 @@ $(document).ready(function() {
     $(".js-members").text(response.data.members)
   });
 
-  $.getJSON("https://api.meetup.com/Barcelona-Free-Software/events?desc=true&photo-host=public&page=20&sig_id=103310792&status=past&fields=photo_album&sig=f3ef85dfc4deeae26cc8b29b6bba906dd05cb3fd&callback=?", function(response) {
-    var meetups = response.data.slice(0, 20);
+  $.getJSON("https://api.meetup.com/barcelona-free-software/events?desc=true&photo-host=public&page=20&sig_id=103310792&status=upcoming%2Cpast&sig=6753a3f52681dd68a724d452abda5a3292fdb2ea&callback=?", function(response) {
+    var upcomingMeetups = response.data.filter(function(d) { return d.status  === 'upcoming'});
+    var pastMeetups = response.data.filter(function(d) { return d.status  === 'past'});
 
-    meetups.forEach(function(d) {
-      var date = new Date(d.created);
+    if (upcomingMeetups) {
+      $('.upcomingMeetups')
+        .append('<h2 class="u-fontMono">Upcoming meetups</h2>')
+        .append('<ul class="meetupList"></ul>');
+
+      printMeetups(upcomingMeetups, '.upcomingMeetups');
+    }
+
+    printMeetups(pastMeetups, '.lastMeetups');
+  });
+
+  function printMeetups(data, selector) {
+    data.forEach(function(d) {
+      var date = new Date(d.time);
       var day = date.getUTCDate();
       var month = date.getUTCMonth() + 1; //months from 1-12
       var year = date.getUTCFullYear();
 
-      $(".lastMeetups")
-        .append('<li><a class="u-marginRight" href="'+ d.link + '">' + d.name + '</a><small>' + day + '/' + month + '/' + year  + ' @' + d.venue.name + '</small></li>')
+      $(selector + ' ul')
+        .append('<li><a class="u-marginRight" href="'+ d.link + '">' + d.name + '</a><small>' + day + '/' + month + '/' + year  + ' @<a class="muted" href="https://www.openstreetmap.org/directions?from=&to=' + d.venue.lat + '%2C' + d.venue.lon + '#map=16/' + d.venue.lat + '/' + d.venue.lon + '">' + d.venue.name + '</a></small></li>')
     });
-  });
+  }
 });
